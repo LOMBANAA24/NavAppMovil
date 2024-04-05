874ae5747795b97d60ecac4e41366e4d5db919f6
Nicolle <marqueznicolle421@gmail.com>
2024-04-05 16:06:01 -0500

    feat(StackNavigation): installation complete

---
+++ labreactnativewithclinavigation/src/App.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Text>App</Text>
+        </NavigationContainer>
+    )
+}
+
+export default App;
